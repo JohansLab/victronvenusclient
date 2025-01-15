6d8709f6b2771144f778f82ca258bd9f46e57c91
Johan du Plessis <johan@epicwin.co.za>
2025-01-15 14:13:19 +0200

    initial implementation

---
+++ src/victronvenusclient/__about__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present Johan du Plessis https://github.com/johanslab
+#
+# SPDX-License-Identifier: MIT
+"""
+Version information.
+"""
+
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Johan du Plessis https://github.com/johanslab
+#
+# SPDX-License-Identifier: MIT
